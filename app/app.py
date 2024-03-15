241f58ca669439b660409a5b8a1a09c2dc41f617
Alexanderkaizen <alexanderespinalkaizen@gmail.com>
2024-03-15 01:02:31 -0400

    Pagina principal

--- app/app.py
+++ app/app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 #Pagina principal
 
 @app.route('/')
-def hello():
+def index():
     return "<h1>Hello, World!</h1>" 
 
 # Esto imprime tu nombre y edad
@@ -17,23 +17,8 @@ def user(name = None, edad = None):
     else:
         return f"<h1>Bienvenido user {name} tienes {edad} años</h1>" 
     
-# Esto es una calculadora basica
 
-@app.route('/calculadora/<operation>/<int:num1>/<int:num2>/<int:num3>')
-def calculadora(num1,num2,num3,operation):
-    if operation == "suma":
-        return f" La suma de como resultado: {num1 + num2 + num3}"
-    elif operation == "resta":
-        return f" La resta da como resultado: {num1 - num2 - num3}"
-    elif operation == "multiplicacion":
-        return f" La multiplicacion da como resultado: {num1 * num2  *num3}"
-    elif operation == "divicion":
-        return f" La divicion de como resultado: {num1 / num2 / num3}"
-    elif operation == "elevado":
-        return f" La elevacion da como resultado: {num1 ** num2 **num3}"
-    
         
-
 #Login 
 
 @app.route('/login')
@@ -42,5 +27,5 @@ def login():
 
 
 if __name__  == '__main__':
-    app.run( debug=True)
+    app.run(debug=True)
 
